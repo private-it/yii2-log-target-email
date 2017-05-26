dcf1b3131acad8f431b4ab482a89b3ce15fc1a1b
Rendol <sapegin@private-it.ru>
2017-05-26 20:22:31 +0700

    Add exclude patterns for logs

--- src/EmailTarget.php
+++ src/EmailTarget.php
@@ -8,6 +8,10 @@ use yii\web\NotFoundHttpException;
 
 class EmailTarget extends \yii\log\EmailTarget
 {
+    public $excSubject;
+    public $excStatusCode = [403, 404];
+    public $excErrorCode = [];
+
     /**
      * @inheritdoc
      */
@@ -16,6 +20,28 @@ class EmailTarget extends \yii\log\EmailTarget
         if (isset($this->messages[0], $this->messages[0][0])) {
             /** @var NotFoundHttpException $message */
             $message = $this->messages[0][0];
+
+            // Exclude by subject
+            if ($this->excSubject) {
+                if (preg_match($this->excSubject, $this->message['subject']) !== false) {
+                    return;
+                }
+            }
+
+            // Exclude by http status code
+            if (sizeof($this->excStatusCode) > 0) {
+                if (in_array($message->statusCode, $this->excStatusCode)) {
+                    return;
+                }
+            }
+
+            // Exclude by error code
+            if (sizeof($this->excErrorCode) > 0) {
+                if (in_array($message->getCode(), $this->excErrorCode)) {
+                    return;
+                }
+            }
+
             if ($message instanceof HttpException) {
                 $this->message['subject'] = strtr($this->message['subject'], [
                     '{message}' => '[' . $message->statusCode . '] ' . $message->getMessage(),
